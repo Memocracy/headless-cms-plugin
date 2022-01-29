71350827d4c38c23bcb736f373413390d4a0a129
Konrad Fedorczyk <contact@realhe.ro>
2022-01-29 15:36:50 +0100

    Remove unused styles for admin

--- headless-cms-plugin.php
+++ headless-cms-plugin.php
@@ -46,8 +46,8 @@ $pluginName = 'WpMPB';
 $plugin = new \RealHero\Memocracy\Bootstrap($pluginName, $assetsUrl, __DIR__, '1.0.0');
 
 // Add components.
-$plugin->registerComponent('frontendAssets', '\RealHero\Memocracy\Components\FrontendAssets');
-$plugin->registerComponent('adminAssets', '\RealHero\Memocracy\Components\AdminAssets');
+// $plugin->registerComponent('frontendAssets', '\RealHero\Memocracy\Components\FrontendAssets');
+// $plugin->registerComponent('adminAssets', '\RealHero\Memocracy\Components\AdminAssets');
 $plugin->registerComponent('i18n', '\RealHero\Memocracy\Components\I18n');
 $plugin->registerComponent('publicationsCpt', '\RealHero\Memocracy\Components\PublicationsCpt');
 $plugin->registerComponent('teamCpt', '\RealHero\Memocracy\Components\TeamCpt');
